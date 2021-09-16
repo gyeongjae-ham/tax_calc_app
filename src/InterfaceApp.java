3fc65b3de2a3330c463710f1c446544446a3401a
hiyee-gj <gyeongjae.h.dev@gmail.com>
2021-09-16 14:59:37 +0900

    JAVA interface content explain text add.

--- src/InterfaceApp.java
+++ src/InterfaceApp.java
@@ -21,7 +21,8 @@ class AdvancedPrint implements Printable {
 public class InterfaceApp {
     public static void main(String[] args) {
         // 더하기 기능이 있는 클래스를 만들어주세요.라고 외주했을 때 서로 다른 형식의 클래스를 생성할 수 있음(협업할 때도)
-        Printable c = new AdvancedPrint(); // 같은 interface의 호환성을 제공하기도 한다
+        Printable c = new AdvancedPrint(); // 하나의 객체가 여러 타입을 가질 수 있음(다형성(polymorphism))
+        // 같은 interface의 호환성을 제공하기도 한다
         c.print();
     }
 }
